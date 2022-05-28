73ccdd497ea54b5a246b3ec5b7e429cbbeeba186
emeraldhycient <igwezehycient86@gmail.com>
2022-05-28 14:23:03 +0100

    [add] started working on marketplace

--- src/pages/market/index.jsx
+++ src/pages/market/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../../components/layout";
 import Navbar from "../../components/market/navbar/navbar";
+import Product from "../../components/market/product";
 
 function Index() {
   return (
@@ -10,8 +11,37 @@ function Index() {
     >
       <Navbar />
       <div className="grid grid-cols-1 md:grid-cols-6 gap-4 mt-8">
-        <div className="bg-gray-200 h-screen"></div>
-        <div className="col-span-5 bg-gray-200 h-screen"></div>
+        <div className="bg-gray-200 h-screen ml-1"></div>
+        <div className="col-span-5  h-screen pr-2 md:pr-4">
+          <div className="">
+            <select className="bg-white border mx-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-gray-500">
+              <option>Sort by</option>
+              <option>Lenses</option>
+              <option>Frames</option>
+              <option>Accessories</option>
+              <option>Lab Equipment</option>
+              <option>Optical Materials</option>
+            </select>
+            <select className="bg-white border mx-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-gray-500">
+              <option>Delivery options</option>
+              <option>Lenses</option>
+              <option>Frames</option>
+              <option>Accessories</option>
+              <option>Lab Equipment</option>
+              <option>Optical Materials</option>
+            </select>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+          </div>
+        </div>
       </div>
     </Layout>
   );
