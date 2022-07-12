43fe06c3bf6f9ece93f7f697846a0e08b0626b41
emeraldhycient <igwezehycient86@gmail.com>
2022-07-12 22:26:48 +0100

    [add] added phones restriction on mobiles for sellers dashboard

--- src/components/seller/Layout/Layout.jsx
+++ src/components/seller/Layout/Layout.jsx
@@ -1,45 +1,29 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
-  const [isOnMobile, setisOnMobile] = useState(false);
-
-  const deviceType = () => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-      setisOnMobile(true);
-    } else if (
-      /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-        ua
-      )
-    ) {
-      setisOnMobile(true);
-    }
-    setisOnMobile(false);
-  };
+  const navigate = useNavigate();
 
   useEffect(() => {
-    deviceType();
+    if (screen.orientation !== null) {
+      alert("Please use desktop version to access this page xx");
+      navigate("/market");
+    }
   }, []);
 
   return (
     <section className="h-screen w-full">
-      {isOnMobile ? (
-        <div className="h-full w-full">
-          <h1>please view this page on a desktop for optimum performance</h1>
+      <div className="grid grid-cols-1 md:grid-cols-7 h-screen w-full">
+        <div className="bg-amber-500">
+          <Sidebar />
         </div>
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-7 h-screen w-full">
-          <div className="bg-amber-500">
-            <Sidebar />
-          </div>
-          <div className="col-col-6 h-full w-[85.5vw] overflow-y-auto">
-            <Header />
-            {children}
-          </div>
+        <div className="col-col-6 h-full w-[85.5vw] overflow-y-auto">
+          <Header />
+          {children}
         </div>
-      )}
+      </div>
     </section>
   );
 }
